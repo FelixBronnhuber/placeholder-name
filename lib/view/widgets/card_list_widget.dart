96c45a09530a4f2340ee8884af472eaadc06cea3
Felix Bronnhuber <febrolix@gmail.com>
2023-05-15 14:11:13 +0200

    Refactoring and minor UI changes.

--- lib/view/widgets/card_list_widget.dart
+++ lib/view/widgets/card_list_widget.dart
@@ -11,37 +11,40 @@ class CardListWidget extends StatefulWidget {
   CardListWidgetState createState() => CardListWidgetState();
 }
 
-class CardListWidgetState extends State<CardListWidget> with TickerProviderStateMixin{
-
+class CardListWidgetState extends State<CardListWidget>
+    with TickerProviderStateMixin {
   Widget _buildCardItem(api.Card card) {
     bool status = false;
-    AnimationController _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 250));
-    Animation<double> _animation = Tween<double>(begin: 0, end: 1.571).animate(_controller);
+    AnimationController controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 250));
+    Animation<double> animation =
+        Tween<double>(begin: 0, end: 1.571).animate(controller);
 
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 60.0),
       child: Center(
-          child:AnimatedBuilder(animation: _animation,
-            builder: (context, child) {
-              return Transform.rotate(
-                angle: _animation.value,
-                child: InkWell(
-                  onTap: () {
-                    if(status)
-                      _controller.forward();
-                    else
-                      _controller.reverse();
-                    status = !status;
-                  },
-                  child: Image.network(
-                      card.imageURIS?.png ??
-                          card.cardFaces.first.imageURIS!.png,
-                      scale: 1.0),
-                ),
-              );
-            },
-          ),
+        child: AnimatedBuilder(
+          animation: animation,
+          builder: (context, child) {
+            return Transform.rotate(
+              angle: animation.value,
+              child: InkWell(
+                onTap: () {
+                  if (status) {
+                    controller.forward();
+                  } else {
+                    controller.reverse();
+                  }
+                  status = !status;
+                },
+                child: Image.network(
+                    card.imageURIS?.png ?? card.cardFaces.first.imageURIS!.png,
+                    scale: 1.0),
+              ),
+            );
+          },
         ),
+      ),
     );
   }
 
@@ -49,21 +52,20 @@ class CardListWidgetState extends State<CardListWidget> with TickerProviderState
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(children: <Widget>[
-        ListView.separated(
+        ListView.builder(
           shrinkWrap: true,
           physics: const NeverScrollableScrollPhysics(),
           scrollDirection: Axis.vertical,
           itemCount: widget._cardList.length,
-          separatorBuilder: (context, index) {
-            return const Divider();
-          },
           itemBuilder: (BuildContext context, int index) {
             api.Card data = widget._cardList[index];
             return InkWell(
               onTap: () {
                 widget._function(data);
               },
-              child: _buildCardItem(data),
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: _buildCardItem(data)),
             );
           },
         ),
