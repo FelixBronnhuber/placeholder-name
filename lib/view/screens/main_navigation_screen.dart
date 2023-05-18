1b27282d7e9772cfb7d5e424bcc57f0dff43eb61
Felix Bronnhuber <febrolix@gmail.com>
2023-05-18 21:15:58 +0200

    Dragging left from the first screen now navigates to the last screen and vice-versa.

--- lib/view/screens/main_navigation_screen.dart
+++ lib/view/screens/main_navigation_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:placeholder_name/view/screens/card_search_screen.dart';
 import 'package:placeholder_name/view/screens/deck_statistics_screen.dart';
 import 'package:placeholder_name/view/screens/deck_view_screen.dart';
@@ -12,11 +12,9 @@ class MainNavigationScreen extends StatefulWidget {
 }
 
 class MainNavigationScreenState extends State<MainNavigationScreen> {
-  final _controller = PageController(
-    initialPage: 0,
-    viewportFraction: 1.0,
-  );
+  int _currentScreen = 1;
 
+  late PageController _controller;
   late DecksListScreen decksListScreen;
   late CardSearchScreen cardSearchScreen;
   late DeckViewScreen deckViewScreen;
@@ -25,6 +23,11 @@ class MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   void initState() {
     super.initState();
+
+    _controller = PageController(
+      initialPage: _currentScreen,
+      viewportFraction: 1.0,
+    );
 
     decksListScreen = DecksListScreen(parent: this);
     cardSearchScreen = CardSearchScreen(parent: this);
@@ -45,11 +48,28 @@ class MainNavigationScreenState extends State<MainNavigationScreen> {
       scrollDirection: Axis.horizontal,
       pageSnapping: true,
       children: [
+        Container(color: Colors.black),
         decksListScreen,
         cardSearchScreen,
         deckViewScreen,
-        deckStatisticsScreen
+        deckStatisticsScreen,
+        Container(color: Colors.black)
       ],
+      onPageChanged: (nextPage) {
+        if (nextPage == 0) {
+          _controller.animateToPage(4,
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.fastOutSlowIn);
+        } else if (nextPage == 5) {
+          _controller.animateToPage(1,
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.fastOutSlowIn);
+        }
+
+        setState(() {
+          _currentScreen = nextPage;
+        });
+      },
     );
   }
 }
